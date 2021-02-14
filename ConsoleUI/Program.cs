2c97b38d68b1e1f2e774fff22de468eff5352bb0
firatdisli <78796132+firatdisli@users.noreply.github.com>
2021-02-14 22:09:03 +0300

    Add files via upload
    EntityFrame eklenip Derlenmiş Hali github yüklenemediğinden sadece gerekli klasör ve dosyaları ekledim

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Business.Concrete;
+
+using DataAccess.Concrete.EntityFramework;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CarManager carMamager = new CarManager(new EfCarDal());
+
+            foreach(var car in carMamager.GetAll())
+            {
+                Console.WriteLine(car.Id+" "+car.ModelYear+" "+car.DailyPrice+" "+car.Description);
+            }
+
+
+            Car car1 = new Car() {Id=10,CarName="F", BrandId=3,ColorId=2,DailyPrice=10,ModelYear=2012,Description="Eklenemeyen"};
+            Car car2 = new Car() { Id = 11, CarName = "Ford Fiesta", BrandId = 3, ColorId = 2, DailyPrice = 0, ModelYear = 2012, Description = "Eklenemeyen" };
+            Car car3 = new Car() { Id = 12, CarName = "Ford Fiesta", BrandId = 3, ColorId = 2, DailyPrice = 100, ModelYear = 2012, Description = "Aile Arabası" };
+
+            //Bir Önceki çalıştırmada eklenen değerler primarykey özelliğinden dolayı, comment yapılmalı
+           // carMamager.Delete(car3);
+            carMamager.Add(car1);
+            carMamager.Add(car2);
+           // carMamager.Add(car3);
+            //Delete metodu car nennesi istediğinden şimdilik tüm özelliklerini göndermek gerekli
+            //carMamager.Add(new Car() {Id=3,BrandId=2,ColorId=3,ModelYear=2017,DailyPrice=250,Description="Lüx Araba",CarName="Ford Mondeo" });
+            carMamager.Update(new Car() { Id = 4, BrandId = 3, ColorId = 2, ModelYear = 2014, DailyPrice = 250, Description = "Lüx Araba", CarName = "Ford Mondeo" });
+
+            Console.WriteLine("---------ekleme güncelleme ve silmeden sonra arabalar------");
+            foreach (var car in carMamager.GetAll())
+            {
+                Console.WriteLine(car.Id + " " + car.ModelYear + " " + car.DailyPrice + " " + car.Description);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            if (entity.CarName.Length > 2 && entity.DailyPrice > 0)
+            {
+                using (RentCarContext context = new RentCarContext())
+                {
+                    var addedEntity = context.Entry(entity);
+                    addedEntity.State = EntityState.Added;
+                    context.SaveChanges();
+                    Console.WriteLine("Ekleme Başarılı");
+                }
+            }
+            else
+                Console.WriteLine(entity.Id+ "id li araba ismi 2 karekterden uzun, price 0dan büyük olmalı");
+        }
+        public void Delete(Car entity)
+        {
+            using (RentCarContext context = new RentCarContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+       
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            using (RentCarContext context = new RentCarContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using(RentCarContext context=new RentCarContext())
+            {
+                return filter == null
+                    ? context.Set<Car>().ToList()
+                    : context.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Car entity)
+        {
+            using (RentCarContext context = new RentCarContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
